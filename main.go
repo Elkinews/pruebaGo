a9678547b96164755d53a8ff4ab6d7e6da8d2894
elkinews <elkinews@pop-os.localdomain>
2022-01-27 16:31:30 -0500

    class #25, go curse Platzi

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"example.com/prueba1/selfFuncs"
-)
+type Figuras2D interface {
+	Area() float64
+}
 
+type Cuadrado struct {
+	Base float64
+}
+type Rectangulo struct {
+	Base   float64
+	Altura float64
+}
+
+func (c Cuadrado) Area() float64 {
+	return c.Base * c.Base
+}
+
+func (r Rectangulo) Area() float64 {
+	return r.Base * r.Base
+}
+func Calcular(f Figuras2D) {
+	fmt.Printf("Area %2f \n", f.Area())
+}
 func main() {
+	myCuadrado := Cuadrado{Base: 2}
+	myRectangulo := Rectangulo{Base: 2.111, Altura: 4.434}
+	Calcular(myCuadrado)
+	Calcular(myRectangulo)
 
-	var a int = 1
-
-	b := &a
-	fmt.Println(*b)
-	*b = 100
-	fmt.Println(a, b)
-
-	myPC := selfFuncs.Pc{Ram: 12, Disk: 200, Brand: "asus"}
-	laptop := selfFuncs.Pc{Ram: 8, Disk: 100, Brand: "dell"}
-	fmt.Println(myPC)
-	myPC.Ping()
-	myPC.DuplicateRAM()
-	fmt.Println(myPC)
-	laptop.DuplicateRAM()
-	fmt.Println(laptop)
+	//Lista de interfaces
+	myInterface := []interface{}{"hola", 12, 14.9}
+	fmt.Println(myInterface...)
 }
